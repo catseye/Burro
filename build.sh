2f035f40ed47961d5c5745a8311bda1f5c25d824
Cat's Eye Technologies <cpressey@catseye.tc>
2014-04-21 14:29:26 +0100

    Simplify build system.  Don't build if ghc not found.

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ x`which ghc` = x -a x`which runhugs` = x ]; then
+    echo "Neither ghc nor runhugs found on search path."
+    exit 1
+fi
+
+if [ x`which ghc` = x  ]; then
+    echo "ghc not found on search path.  Use Hugs to run."
+    exit 0
+fi
+
+ghc --make src/Burro.lhs
+
+# Burro${O}: Burro.lhs
+# 	${HC} ${HCFLAGS} -c $*.lhs
+# 
+# ${PROG}: ${OBJS}
+# 	${HC} -o ${PROG} -O ${OBJS}
+# 	strip ${PROG}
