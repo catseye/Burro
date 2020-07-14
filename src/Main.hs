4af850574b88a8ca63e6599cce574d8c80aa1192
Chris Pressey <cpressey@catseye.tc>
2020-07-14 11:29:16 +0100

    Add rudiementary Burro debugger and rudimentary example programs.

--- src/Main.hs
+++ src/Main.hs
@@ -2,13 +2,19 @@ module Main where
 
 import System.Environment
 import Language.Burro
+import qualified Language.Burro.Debugger as Debugger
 
 main = do
     args <- getArgs
     case args of
-        [fileName] -> do
+        ["run", fileName] -> do
             c <- readFile fileName
             burroText <- readFile fileName
             putStrLn $ show $ interpret burroText
+        ["debug", fileName] -> do
+            c <- readFile fileName
+            burroText <- readFile fileName
+            state <- Debugger.interpret burroText
+            putStrLn $ show $ state
         _ -> do
-            putStrLn "Usage: burro <filename.burro>"
+            putStrLn "Usage: burro (run|debug) <filename.burro>"
